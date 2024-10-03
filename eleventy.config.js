4bb92d70e984c9a6aecda942e7b2915ed14ae540
December G <dgarnetsmith@gmail.com>
2024-10-02 20:15:42 -0500

    Display favicon

--- eleventy.config.js
+++ eleventy.config.js
@@ -24,10 +24,8 @@ export default async function(eleventyConfig) {
 		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl");
 	eleventyConfig
 		.addPassthroughCopy({
-			"img/favicon": "/"
+			"./public/img/favicon": "/"
 		})
-		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl");
-
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
 
